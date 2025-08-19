a29ecfee4e522115e9d37c9e451334f4d0625b5e
Arvan Ardana <113536365+arvardy184@users.noreply.github.com>
2025-08-19 23:31:09 +0700

    fix(dockerfile): fix error in dockerfile

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,26 +1,28 @@
-import { string } from "joi";
-import nodemailer from "nodemailer";
+import { string } from 'joi';
+import nodemailer from 'nodemailer';
 
-export async function sendOTPEmail(to: string, otp: string){
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        secure: true,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        },
-    });
+export async function sendOTPEmail(to: string, otp: string) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
-    let info = await transporter.sendMail({
-        from: process.env.SMTP_FROM, //sender
-        to: to, //reciever
-        subject: "OTP Verification Code",
-        text: `Hi there,
+  let info = await transporter.sendMail({
+    from: process.env.SMTP_FROM, //sender
+    to: to, //reciever
+    subject: 'OTP Verification Code',
+    text: `Hi there,
 Your verification code for Allertify is:${otp}
 This code is valid for 5 minutes. 
 Please do not share this code with anyone to keep your account secure.
 Thanks,
 The Allertify Team`,
-    });
-    console.log("Code has been sent: %s", info.response);
+  });
+  console.log('Code has been sent: %s', info.response);
 }
